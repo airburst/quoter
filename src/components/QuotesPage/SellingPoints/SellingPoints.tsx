1b811f76683d5918cd9c248155d059ffd02124ce
Joseph Markus <joseph.markus@simplybusiness.co.uk>
2023-05-25 18:18:35 +0100

    Add CircularIcon component

--- src/components/QuotesPage/SellingPoints/SellingPoints.tsx
+++ src/components/QuotesPage/SellingPoints/SellingPoints.tsx
@@ -1,16 +1,19 @@
-import { Text } from "@twilio-paste/text";
+import { Heading } from "@twilio-paste/heading";
 import { Feature } from "../Feature";
 import { InsurerLogos } from "../InsurerLogos";
 import { Section } from "@components/Section";
+import { FileLinesIcon, HeadsetIcon } from "@components/Icon";
 import styles from "./SellingPoints.module.css";
 
 export const SellingPoints = () => {
   return (
     <Section color="light">
       <div className={styles.container}>
-        <Text as="h3">Simply business is different.</Text>
+        <Heading variant="heading20" as="h2">
+          Simply business is different.
+        </Heading>
         <Feature
-          icon="./feature-icon.png"
+          icon={<HeadsetIcon />}
           title="Access top rated insurance carriers"
           description="Tailored small business insurance quotes from our curated panel of a-rated carriers."
         >
@@ -18,17 +21,17 @@ export const SellingPoints = () => {
         </Feature>
 
         <Feature
-          icon="./feature-icon.png"
+          icon={<FileLinesIcon />}
           title="Putting you in control"
           description="Transparent quotes allow you to compare prices, coverage and buy online."
         />
         <Feature
-          icon="./feature-icon.png"
+          icon={<FileLinesIcon />}
           title="Expert agent support"
           description="Support every step of the way with our brilliant licensed insurance agents."
         />
         <Feature
-          icon="./feature-icon.png"
+          icon={<FileLinesIcon />}
           title="Instant access to your COIs"
           description="Immediately access your certificate of insurance and documents via your online account."
         />
