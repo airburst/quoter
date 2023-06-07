675f1575a9e4f0d9bd923cd10a82755cb3ca583c
Joseph Markus <joseph.markus@simplybusiness.co.uk>
2023-06-07 12:18:18 +0100

    Fix random ID returning duplicates

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -5,7 +5,7 @@ export const delay = (ms: number = DEFAULT_DELAY_IN_MS) =>
   new Promise(resolve => setTimeout(resolve, ms));
 
 export const randomId = () => {
-  const RANDOM_MULTIPLIER = 100;
+  const RANDOM_MULTIPLIER = 1000;
 
   return Math.floor(Math.random() * RANDOM_MULTIPLIER).toString();
 };
